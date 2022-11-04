adecb1e06f2d535ecda24244a168677fa26e14d7
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-11-04 10:44:31 -0300

    Feat: Mobile app created

---
+++ mobile/src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base"
+
+export function SignIn() {
+    return (
+        <Center bgColor="gray.900" flex={1}>
+            <Text color="white" fontSize={24}>
+                Sign in
+            </Text>
+        </Center>
+    )
+}
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+interface HomeProps {
+    count: number
+}
+
+export default function Home(props: HomeProps) {
+    return (
+        <>
+            <h1>Contagem: </h1>
+            <h2>{props.count}</h2>
+        </>
+    )
+}
+
+// Camada que roda do lado do servidor do next:
+export const getServerSideProps = async () => {
+    const response = await fetch("http://localhost:3333/pools/count")
+    const data = await response.json()
+
+    console.log(data)
+
+    return {
+        props: {
+            count: data.count,
+        },
+    }
 }
