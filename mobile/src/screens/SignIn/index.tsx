921a71b8b7d22c8b0af18a7ca1c4c701b94f3fe3
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-11-04 17:52:28 -0300

    Feat: class 3 stopped at 11:07

--- mobile/src/screens/SignIn/index.tsx
+++ mobile/src/screens/SignIn/index.tsx
@@ -1,11 +1,14 @@
 import { Center, Text } from "native-base"
 
+import Logo from "../../assets/logo.svg"
+
 export function SignIn() {
     return (
         <Center bgColor="gray.900" flex={1}>
-            <Text color="white" fontSize={24}>
+            <Logo />
+            {/* <Text color="white" fontSize={24} fontFamily="heading">
                 Sign in
-            </Text>
+            </Text> */}
         </Center>
     )
 }
